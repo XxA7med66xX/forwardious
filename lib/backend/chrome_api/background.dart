162ab2167e739ccb3b9e577b70ce665b232d22b6
XxA7med66xX <XxAhmed66xX@proton.me>
2024-06-24 10:29:42 +0300

    Apply a regex pattern filter to match valid YouTube URLs
    Previously the contains() function was used, which caused a redirect for any URL containing the word "youtube" which was not the intended behavior. Now, this issue should be fixed to redirect only the matched YouTube URLs.

--- lib/backend/chrome_api/background.dart
+++ lib/backend/chrome_api/background.dart
@@ -1,27 +1,35 @@
 import 'package:chrome_extension/action.dart';
 import 'package:chrome_extension/runtime.dart';
-import 'package:chrome_extension/storage.dart';
 import 'package:chrome_extension/tabs.dart';
 import 'package:forwardious/backend/chrome_api/tabs_behaviour.dart';
+import 'package:forwardious/backend/youtube/url_processing.dart';
 
 main() async {
   //Open popup of the extention once you
   //install it to load the api of invidious
-  //and set first instance of invidious as 
+  //and set first instance of invidious as
   //default instance.
   chrome.runtime.onInstalled.listen((event) {
     chrome.action.openPopup(OpenPopupOptions());
   });
-  
+
   chrome.tabs.onUpdated
       .where((tab) => tab.changeInfo.url?.contains('youtube') ?? false)
-      .listen((tab) async {
-    TabsBehaviour.behaviour(tab.changeInfo.url!, tab.tabId);
-  });
+      .listen(
+    (tab) {
+      if (UrlProcessing.hasMatch(tab.changeInfo.url!)) {
+        TabsBehaviour.behaviour(tab.changeInfo.url!, tab.tabId);
+      }
+    },
+  );
 
   chrome.tabs.onCreated
       .where((tab) => tab.url?.contains('youtube') ?? false)
-      .listen((tab) {
-    TabsBehaviour.behaviour(tab.url!, tab.id!);
-  });
+      .listen(
+    (tab) {
+      if (UrlProcessing.hasMatch(tab.url!)) {
+        TabsBehaviour.behaviour(tab.url!, tab.id!);
+      }
+    },
+  );
 }
--- lib/backend/youtube/url_processing.dart
+++ lib/backend/youtube/url_processing.dart
@@ -1,9 +1,28 @@
+import 'package:forwardious/backend/youtube/channel_id.dart';
 import 'package:forwardious/backend/youtube/id.dart';
+import 'package:forwardious/backend/youtube/playlist_id.dart';
 import 'package:forwardious/backend/youtube/url_type.dart';
 import 'package:forwardious/backend/modules/video.dart';
+import 'package:forwardious/backend/youtube/video_id.dart';
 
 class UrlProcessing extends Video {
   ///A class that processes the YouTube URL
   ///and extracts its ID and type (such as /channel, /playlist, ...etc).
   UrlProcessing(String url) : super(url: url,id: UrlId.getId(url),type: UrlType.getType(url));
+
+  static bool hasMatch(String url) {
+    final List<RegExp> patterns = [
+    ...VideoId.patterns,
+    ...PlaylistId.patterns,
+    ...ChannelId.patterns,
+  ];
+
+    for (var pattern in patterns) {
+      if (pattern.hasMatch(url)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
